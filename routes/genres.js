5c66894d552296ec07497c15dd240b00beee81a8
Anyim Obinna <obinnanyim@gmail.com>
2020-09-02 18:32:36 +0100

    Removing Try Catch Blocks

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const asyncMiddleware = require('./middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 //const Joi = require('joi');
@@ -6,24 +7,19 @@ const mongoose = require('mongoose');
 const express = require('express');
 const routerr = express.Router();
 
-routerr.get('/', async(req, res, next) => {
-    try {
-        const genre = await Genre.find().sort('name');
-        res.send(genre);
-    } catch (ex) {
-        next(ex);
-    }
+routerr.get('/', asyncMiddleware(async(req, res) => {
+    const genre = await Genre.find().sort('name');
+    res.send(genre);
+}));
 
-});
-
-routerr.get('/:id', async(req, res) => {
+routerr.get('/:id', asyncMiddleware(async(req, res) => {
     const genre = await Genre.findById(req.params.id);
     //const genre = genres.find(g => g.id === parseInt(req.params.id));
     if (!genre) return res.status(400).send(`The genre with ID ${req.params.id} is not found!`)
     res.send(genre);
-})
+}));
 
-routerr.post('/', auth, async(req, res) => {
+routerr.post('/', auth, asyncMiddleware(async(req, res) => {
 
     //  const result = validateGenre(req.body);
     const { error } = validate(req.body);
@@ -33,9 +29,9 @@ routerr.post('/', auth, async(req, res) => {
     await genre.save();
     res.send(genre);
 
-})
+}));
 
-routerr.put('/:id', auth, async(req, res) => {
+routerr.put('/:id', auth, asyncMiddleware(async(req, res) => {
 
     const { error } = validate(req.body);
     if (error) return res.status(404).send(error.details[0].message)
@@ -47,9 +43,9 @@ routerr.put('/:id', auth, async(req, res) => {
 
     // genre.name = req.body.name;
     res.send(genre);
-});
+}));
 
-routerr.delete('/:id', [auth, admin], async(req, res) => {
+routerr.delete('/:id', [auth, admin], asyncMiddleware(async(req, res) => {
     // look up the course
     const genre = await Genre.findByIdAndRemove(req.params.id);
     //const genre = genres.find(c => c.id === parseInt(req.params.id));
@@ -61,6 +57,6 @@ routerr.delete('/:id', [auth, admin], async(req, res) => {
 
     res.send(genre);
     // return same genre
-})
+}));
 
 module.exports = routerr;
