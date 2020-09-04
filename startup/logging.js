26f6328397f30bb643077ddd9edad78b8167bde3
Anyim Obinna <obinnanyim@gmail.com>
2020-09-04 11:29:06 +0100

    Showing unhandled Exceptions on the console

--- startup/logging.js
+++ startup/logging.js
@@ -8,8 +8,10 @@ module.exports = function() {
     //     winston.error(ex.message, ex);
     //     process.exit(1);
     // });
-
-    winston.handleExceptions(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+    winston.handleExceptions(
+        new winston.transports.Console({ colorize: true, prettyPrint: true }),
+        new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+    );
 
     process.on('unhandledRejection', (ex) => {
         throw ex;
