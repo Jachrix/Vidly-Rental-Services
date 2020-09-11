dd9c46c361f742aa083331f2988bc8fd3f98b63b
Anyim Obinna <obinnanyim@gmail.com>
2020-09-11 09:29:14 +0100

    Populating the Test DB

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,15 +1,27 @@
-const { expectCt } = require('helmet');
 const request = require('supertest');
+const { Genre } = require('../../models/genre');
 
 let server;
 
 describe('/api/genres', () => {
     beforeEach(() => { server = require('../../app.js'); });
-    afterEach(() => { server.close(); });
+    afterEach(async() => {
+        server.close(),
+            await Genre.remove({})
+    });
     describe('GET /', () => {
         it('should get all genres', async() => {
+            await Genre.collection.insertMany([
+                { name: 'genre1' },
+                { name: 'genre2' }
+            ]);
+            //
             const res = await request(server).get('/api/genres');
             expect(res.status).toBe(200);
+            expect(res.body.length).toBe(2);
+            expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
+            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+
         });
     });
 });
