5e04ac40274456037d73823ce29d41a77d156cae
Anyim Obinna <obinnanyim@gmail.com>
2020-09-12 14:08:42 +0100

    Testing the Auth Middleware

---
+++ tests/integration/auth.test.js
@@ -0,0 +1,49 @@
+const { User } = require('../../models/user');
+const { Genre } = require('../../models/genre');
+const request = require('supertest');
+const winston = require('winston');
+const mongoose = require('mongoose');
+
+
+let server;
+
+describe('auth middleware', () => {
+    beforeEach(() => { server = require('../../app.js') });
+    afterEach(async() => {
+        server.close();
+        await Genre.remove({});
+    });
+
+    let token;
+
+    const exec = () => {
+        return request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send({ name: 'genre1' })
+    }
+    beforeEach(() => {
+        token = new User().generateAuthKey();
+    })
+    it('should return 401 if token is not passed', async() => {
+
+        token = '';
+
+        const res = await exec();
+        expect(res.status).toBe(401);
+    });
+
+    it('should return 400 if token is invalid', async() => {
+
+        token = 'a';
+
+        const res = await exec();
+        expect(res.status).toBe(400);
+    });
+
+    it('should return 200 if token is valid', async() => {
+
+        const res = await exec();
+        expect(res.status).toBe(200);
+    });
+})
