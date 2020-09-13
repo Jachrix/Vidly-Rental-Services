9be2605baf2b4b1c1f7b6f453d0eaf29bd7506d3
Anyim Obinna <obinnanyim@gmail.com>
2020-09-13 08:31:45 +0100

    Refactoring Tests

--- routes/returns.js
+++ routes/returns.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', async(req, res) => {
+router.post('/', (req, res) => {
     if (!req.body.customerId) return res.status(400).send('Customer ID not provided');
     if (!req.body.movieId) return res.status(400).send('Movie ID not provided');
     res.status(401).send('Unauthorized');
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -3,18 +3,28 @@ const { Rental } = require('../../models/rental');
 const { User } = require('../../models/user');
 const mongoose = require('mongoose');
 
-let server;
-let customerId;
-let movieId;
-let rental;
 
 describe('/api/returns', () => {
+
+    let server;
+    let customerId;
+    let movieId;
+    let rental;
+    let token;
+
+    const exec = async() => {
+        return request(server)
+            .post('/api/returns')
+            .set('x-auth-token', token)
+            .send({ customerId, movieId });
+    }
 
     beforeEach(async() => {
         server = require('../../app');
 
         customerId = mongoose.Types.ObjectId();
         movieId = mongoose.Types.ObjectId();
+        token = new User().generateAuthKey();
 
         rental = new Rental({
             customer: {
@@ -37,30 +47,28 @@ describe('/api/returns', () => {
 
 
     it('should return 401 if user is not logged in', async() => {
-        const res = await request(server)
-            .post('/api/returns')
-            .send({ customerId, movieId }); //ES6 wen key and value name thesame
+
+        token = '';
+
+        const res = await exec();
 
         expect(res.status).toBe(401);
     });
 
 
     it('should return 400 if customer ID is not provided', async() => {
-        const token = new User().generateAuthKey();
-        const res = await request(server)
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({ movieId }); //ES6 wen key and value name thesame
+
+        customerId = '';
+
+        const res = await exec();
 
         expect(res.status).toBe(400);
     });
 
     it('should return 400 if movie ID is not provided', async() => {
-        const token = new User().generateAuthKey();
-        const res = await request(server)
-            .post('/api/returns')
-            .set('x-auth-token', token)
-            .send({ customerId }); //ES6 wen key and value name thesame
+        movieId = '';
+
+        const res = await exec();
 
         expect(res.status).toBe(400);
     });
