e0f94504dd1f18a5a10b97a00dc1a7ed706123c0
Anyim Obinna <obinnanyim@gmail.com>
2020-09-14 08:37:53 +0100

    Preparing the App for Heroku

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-require('../startup/prod')(app);
+require('./startup/prod')(app);
 
 
 app.set('view engine', 'pug');
