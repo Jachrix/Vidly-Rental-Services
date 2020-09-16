feb5ba56a48eef463dd64cc3e5e5bdc1efc1593a
Anyim Obinna <obinnanyim@gmail.com>
2020-09-16 02:26:07 +0100

    Listen on PORT

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.set('view engine', 'pug');
 // });
 
 // Environment - Port
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => winston.info(`Listening on port ...... ${port}`));
 
